f85ce1266688b290adbcaea9adeb6d45a4feaa15
chuluq <chuluq@arvis.id>
2024-07-17 08:39:42 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { writeToFile } from "./write.js";
+
+writeToFile("hello.log", "Moch Choirul Chuluq");
+
+console.info("hello world");
---
+++ lodash.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+const name = "MOCH CHOIRUL CHULUQ";
+const result = _.capitalize(name);
+
+console.info(result);
